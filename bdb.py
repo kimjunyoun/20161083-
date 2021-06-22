b722353f33f9403c02bbab25801373f48f43f5cc
kjy <tilehouse0225@naver.com>
2021-06-22 23:19:52 +0900

    first commit

---
+++ bdb.py
@@ -0,0 +1,24 @@
+import pymysql
+
+def insert_data(email, pw):
+    db = pymysql.connect(host='kimjunyoung12345.mysql.pythonanywhere-services.com',
+                    user='kimjunyoung12345', password='kk2563159',
+                    db='kimjunyoung12345$mydb', charset='utf8')
+    c = db.cursor()
+    setdata = (email, pw)
+    c.execute("INSERT INTO user_tb VALUES (%s, %s)", setdata)
+    db.commit()
+
+def get_emailpw(email, pw):
+    print(email, pw)
+    db = pymysql.connect(host='kimjunyoung12345.mysql.pythonanywhere-services.com',
+                    user='kimjunyoung12345', password='kk2563159',
+                    db='kimjunyoung12345$mydb', charset='utf8')
+    c = db.cursor()
+    setdata = (email, pw)
+    c.execute("SELECT * FROM user_tb WHERE email = %s AND pw = %s", setdata)
+    ret = c.fetchone()
+    print(ret)
+
+# insert_data('d@a.com', '4')
+# get_emailpw('d@a.com', '41')
